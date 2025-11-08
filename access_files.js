038c9b3befa80cf21ac6ede1a921018bf4672203
Daphne Wang <daphne.wang974@gmail.com>
2025-11-08 17:05:31 +0100

    add photowebsite files

---
+++ access_files.js
@@ -0,0 +1,297 @@
+let images_details = [
+  {
+    "path": "imgs\\7G9A9910_bw_sm.jpg",
+    "title": "Light in the trees",
+    "location": "Alexandra Palace Park (London)",
+    "date": "2020",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Canne_sm.jpg",
+    "title": "Kanasuk",
+    "location": "Saint Benoit (Reunion Island)",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Chicken1_2_sm.jpg",
+    "title": "Chicken",
+    "location": "Saint Denis (Reunion Island)",
+    "date": "2015",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\EdenBridge_sm.jpg",
+    "title": "Jardin d'Eden",
+    "location": "Etang Sale (Reunion Island)",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Hikers_sm.jpg",
+    "title": "Hikers",
+    "location": "Piton de la Fournaise (Reunion Island)",
+    "date": "2015",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\photons_sm.jpg",
+    "title": "Light.Painting.",
+    "location": "Durham (UK)",
+    "date": "2016",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Let There Be Light_sm.jpg",
+    "title": "Light.",
+    "location": "Reunion Island",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Light-House_sm.jpg",
+    "title": "Light House",
+    "location": "London (UK)",
+    "date": "2020",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\Lone Runner2_sm.jpg",
+    "title": "Lone Runner",
+    "location": "Reunion Island",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Maido2_sm.jpg",
+    "title": "Maido",
+    "location": "Le Maido (Reunion Island)",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\MAURITIUS - 56_sm.jpg",
+    "title": "Waterfall",
+    "location": "Mauritius",
+    "date": "2014",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\Mauritius1_sm.jpg",
+    "title": "Pier 1",
+    "location": "Mauritius",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Mauritius2_sm.jpg",
+    "title": "Pier 2",
+    "location": "Mauritius",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Nature 101_sm.jpg",
+    "title": "Le monde entier est un cactus",
+    "location": "Reunion Island",
+    "date": "2014",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\St Philippe - 110bsi_sm.jpg",
+    "title": "Beware of the wave",
+    "location": "Saint Philippe (Reunion Island)",
+    "date": "2014",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\Tower5_modified_sm.jpg",
+    "title": "Nature will thrive",
+    "location": "Saint Benoit (Reunion Island)",
+    "date": "2014",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\IR_color_5_sm.jpg",
+    "title": "Cherry Blossom 1",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\IR_color_6_sm.jpg",
+    "title": "Cherry Blossom 2",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\IR_color_9_2_sm.jpg",
+    "title": "The Octogon",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_12_2_sm.jpg",
+    "title": "Ghosts 1",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_13_2_sm.jpg",
+    "title": "Hanami",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_24_sm.jpg",
+    "title": "Ghosts 2",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_27_sm.jpg",
+    "title": "Cherry Blossom 3",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_36_2_sm.jpg",
+    "title": "A la francaise",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_37_sm.jpg",
+    "title": "Ghosts 3",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_38_sm.jpg",
+    "title": "Cherry Blossom 4",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_40_sm.jpg",
+    "title": "Entrance to the Bosquet Nord",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\ParcDeSceaux_9_sm.jpg",
+    "title": "Cherry Blossom 5",
+    "location": "Parc de Sceaux (France)",
+    "date": "2025",
+    "in_carousel": false
+  },
+  {
+    "path": "imgs\\092921_11(2)_sm.jpg",
+    "title": "Tulips",
+    "location": "London (UK)",
+    "date": "2021",
+    "in_carousel": true
+  },
+  {
+    "path": "imgs\\092921_14_sm.jpg",
+    "title": "Orchids",
+    "location": "London (UK)",
+    "date": "2021",
+    "in_carousel": false
+  }
+];
+
+function permutation(n) {
+    let permutation = Array
+        .from({ length: n }, (_, i) => i );
+
+    permutation
+        .sort(() => Math.random() - 0.5);
+
+    return permutation;
+}
+
+function update_carousel_item(path, alt='', active=false){
+    let html = '';
+    if (active){
+        html = "<div class='item active'>";
+    }
+    else{
+        html = "<div class='item'>";
+    }
+    html += "<img src='" + path +"' alt='" + alt + "'></img>";
+    html += "</div>" ;
+    document.getElementById('carousel-items').innerHTML += html;
+}
+
+function generate_carousel(){
+    let lenght = images_details.length;
+    let p = permutation(lenght);
+    init_idx = 0;
+    while (images_details[p[init_idx]].in_carousel == false){
+      init_idx ++;  
+    }
+    update_carousel_item(images_details[p[init_idx]].path, images_details[p[init_idx]].title, true);
+    for (idx=init_idx + 1; idx < lenght; idx ++){
+      if (images_details[p[idx]].in_carousel == true){
+        update_carousel_item(images_details[p[idx]].path, images_details[p[idx]].title);
+      }
+    }
+}
+
+function generate_gallery(){
+    let n_imgs = images_details.length;
+    let p = permutation(n_imgs);
+    let n_rows = Math.ceil(n_imgs/3); // 3 images per row
+    let img_idx, img_label;
+    html = '';
+    for (idx_row=0; idx_row<n_rows; idx_row ++){
+        html += "<div class='row'>";
+        for (idx_col=0; idx_col<3; idx_col++){
+            img_idx = idx_row * 3 + idx_col;
+            if (img_idx<n_imgs){
+                img_label = 'img' + String(img_idx);
+                html += "<div class='col-sm-12 col-md-4'>";
+                // Button
+                html += "<button type='button' class='btn btn-link' data-toggle='collapse' data-target='#";
+                html += img_label;
+                html += "'>";
+                html += "<img src='";
+                html += images_details[p[img_idx]].path;
+                html += "' class='img-thumbnail' alt='";
+                html += images_details[p[img_idx]].title;
+                html += "'></button>";
+                // Description
+                html += "<div id='";
+                html += img_label;
+                html += "' class='collapse gallery-item'>";
+                html += "<h4>" + images_details[p[img_idx]].title + "</h4>";
+                html += "<b>Location:</b> " + images_details[p[img_idx]].location + '<br>';
+                html += "<b>Date:</b> " + images_details[p[img_idx]].date + '<br>';
+                html += "</div>";
+
+                html += "</div>";
+            }
+        }
+        html += '</div>';
+    }
+    html += '</div>';
+    document.getElementById('myGallery').innerHTML = html;
+}
+
+function load_content(){
+    generate_carousel();
+    generate_gallery();
+}
+
+window.onload = load_content;
